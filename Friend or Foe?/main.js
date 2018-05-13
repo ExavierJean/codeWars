c1325227f5625bc68f3643e36313f2ddc87cf0fb
ExavierJean <jeanelieexaier@gmail.com>
2018-05-13 01:12:14 -0400

    Update to Friend or Foe

--- Friend or Foe?/main.js
+++ Friend or Foe?/main.js
@@ -1,11 +1,31 @@
 // INSTRUSTIONS
 
 /*
+Make a program that filters a list of strings and returns a list with only your friends name in it.
 
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+Note: keep the original order of the names in the output.
 */
 
 // SOLUTION:
 
+function friend(friends){
+  //your code here
+  var juice = [];
+  for(var i = 0; i < friends.length; i++){
+    if (friends[i].length == 4){
+      juice.push(friends[i]);
+    }
+  }
+  return juice;
+}
+
 // SAMPLE TEST:
 
-// 
+// Test.assertSimilar(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"]);
+// Test.assertSimilar(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"]);
+// Test.assertSimilar(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"]);
+// Test.assertSimilar(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"]);
